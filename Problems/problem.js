dbfc506b51b1664162a546728fa07d9db9e9f391
Arshad Ahmed <arshadahmed10x@gmail.com>
2023-04-02 00:13:23 +0530

    problem

--- Problems/problem.js
+++ Problems/problem.js
@@ -3,3 +3,24 @@
 // 2. Create a simple calculator using switch statement
 
 // 3. switch statement to get the day of the week
+
+// 4.switch statement to get the day count based of a month
+
+// Explaination:
+// > If the month is 1, 3,5, 7, 8, 10, or 12, the number of days in a month is 31.
+// > If the month is 4, 6, 9, or 11, the number of days in that month is 30.
+// > If the month is 2, and the year is not the leap year, the number of days is 28. If the year is the leap year, the number of days is 29.
+// > If the month is not in the valid range (1-12), the default branch executes and sets the dayCount variable to -1, which indicates the invalid month.
+
+
+// 5. Write a JS code to print numbers from 1 to 10
+
+// 6. Write a JS code to print Even numbers in given array
+
+// 7. Write a JS code to delete all occurrence of element in given array
+
+// 8.Write a JS code to find the no of digits in a number
+
+// 9. Write a JS code to print the Fibonacci series for a given value of N
+
+// 10. Write a JS code to count all letters in a word
