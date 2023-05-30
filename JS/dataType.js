4252721a3a2cd5593a2b22ef93055c5c01312c0d
Arshad Ahmed <arshadahmed10x@gmail.com>
2023-05-30 19:24:22 +0530

    sorted

---
+++ JS/dataType.js
@@ -0,0 +1,42 @@
+// >> DATA TYPE
+
+// var myName='arshad ahmed';
+// console.log(myName);
+// console.log(typeof(myName));
+
+// var iamArsh=true;
+// console.log(iamArsh);
+// console.log(typeof(iamArsh));
+
+// ------------------------------------------------------------------------------------------------------------
+
+// >> NULL vs UNDEFINED
+
+// var useless=null;
+// console.log(useless);
+// console.log(typeof(useless)); //object
+
+// var standBy;
+// console.log(standBy);
+// console.log(typeof(standBy)); //undefined
+
+// What is NAN?
+
+// - Property of Global Object
+// - It is a variable in global scope
+// - NOT-A-Number
+
+// console.log("Arsh"-"10x"); // NAN
+// var number=0987654321;
+// console.log(isNaN(number));
+
+// var myName="arshad";
+// console.log(isNaN(myName));
+// if(isNaN(number)){
+//     console.log("Please enter a valid Phone Number")
+// }
+// else{
+//     console.log("Valid Phone Number");
+// }
+
+// ------------------------------------------------------------------------------------------------------------
---
+++ JS/var.js
@@ -0,0 +1,82 @@
+// >> Naming Practice
+
+// var _myName = 'Arshad';
+// var 1myName= 'Arshad'; //invalid
+// var _1myName = 'Arshad';
+// var $myName='Arshad';
+// var myName%= 'Arshad';  //invalid
+// console.log(_myName);
+
+
+
+
+//---------------------------------------------------------------------------------------
+// >> VAR
+
+// var age=25;
+// console.log(age);
+// age=30;
+// console.log(age);
+// var age=35;
+// console.log(age);
+
+
+
+//---------------------------------------------------------------------------------------
+// >> LET
+
+//1.
+
+// let name="arsh";
+// function DemoName(){
+//     let name="10x";
+//     console.log(name);
+// }
+// DemoName();
+// console.log(name);
+
+//2.
+
+// function printNumbers(){
+//     for(let i=0;i<=5;i++){
+//         console.log(i);
+//     }
+//     console.log(i);
+// }
+// printNumbers();
+
+// >> HOISTING
+
+// x=5;
+// console.log(x);
+// var x;
+
+// helloWorld();
+// function helloWorld(){
+//     console.log("Hello World!");
+// }
+
+
+//---------------------------------------------------------------------------------------
+// >> CONST
+
+// const person={name:'arsh',age:30, role: 'developer'};
+// person.age=24;
+// console.log(person);
+
+// const number=[1,2,3,4];
+// number.push(5);
+// console.log(number);
+
+// >> TYPE CONVERSION
+
+// let x;
+// let y;
+// let z;
+
+// x=Number("3.14");
+// y=String("3.14");
+// z=Boolean("abc");
+// console.log(x,typeof x );
+// console.log(y,typeof y );
+// console.log(z,typeof z );
