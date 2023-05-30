a088ad5e1f9968c6d423fb7d24bc5fc9b8101f91
Arshad Ahmed <arshadahmed10x@gmail.com>
2023-05-30 16:57:06 +0530

    variable

---
+++ concept revise/variable.js
@@ -0,0 +1,19 @@
+let name = 'John';
+name='Mark';  
+console.log(name);
+
+let firstName='John';
+let lastName='Doe';
+console.log(firstName + ' ' + lastName);
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+let score = 100;
+let bonus =150;
+console.log(score+bonus);
+
+
+const firstName1= 'Mark';
+// firstName1 = 'Jack'
+console.log(firstName1)
