549bc14355587ee2b4342ce9f1ba37434753a948
Arshad <ahmed.a0611@gmail.com>
2023-07-31 23:48:09 +0530

    new topics

--- JS_Topics/Async_Await.js
+++ JS_Topics/Async_Await.js
@@ -1,3 +1,25 @@
 // Async JS
 
-// 
+const datas=[
+    {name:"arsh",Profession:'UI Developer'},
+    {name:"arshad",Profession:'UI Developer'}
+];
+
+function getDatas(){
+    setTimeout(()=>{
+        let output="";
+        datas.forEach((data,index)=>{
+            output+=`<li>${data.name}</li>`
+        })
+        document.body.innerHTML=output
+    },1000)
+}
+
+function createData(newData,callback){
+    setTimeout(()=>{
+        datas.push(newData);
+        callback();
+    },2000)
+}
+
+createData({name:"Asim", Profession:"UI DEveloper"},getDatas)
