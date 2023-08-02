4eab5ccc5232631f69e5dcf9df06454f26b9db4d
Arshad <ahmed.a0611@gmail.com>
2023-08-02 19:36:07 +0530

    cab

---
+++ JS_Topics/call_appy_blind.js
@@ -0,0 +1,14 @@
+// Call, Apply & Blind
+
+//Problem
+
+let userDetails = {
+    name:'Arshad',
+    age:24,
+    Designation: "UI Developer",
+    printDetails:function(){
+
+    }
+}
+
+userDetails()
