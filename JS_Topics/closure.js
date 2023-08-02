c78d00484ed2712208d8206d9650db116bd52275
Arshad <ahmed.a0611@gmail.com>
2023-08-02 13:12:31 +0530

    closure

---
+++ JS_Topics/closure.js
@@ -0,0 +1,13 @@
+// Closure 
+
+// var sum = function(a){
+// console.log("hello "+a);
+// var c = 4;
+// return function(b){
+//  return a+b+c;
+// }
+// }
+// var store = sum(2);
+// console.log(store(5))
+
+
