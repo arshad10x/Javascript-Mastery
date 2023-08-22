6df9d8530bcea7044dfd54469de951489121344b
Arshad <ahmed.a0611@gmail.com>
2023-08-22 23:24:34 +0530

    changing css

--- demo/dom.js
+++ demo/dom.js
@@ -5,7 +5,14 @@ console.log(a);
 
 // 2. Changing HTML
 
-var b = document.querySelector("h1");
-b.innerHTML = "DOM Started"
+document.querySelector("h1").innerHTML = "DOM Started";
+// var b = document.querySelector("h1");
+// b.innerHTML = "DOM Started";
+
+
 // 3. Changing CSS
+
+a.style.color='white';
+a.style.backgroundColor='green';
+
 // 4. Event Listener
