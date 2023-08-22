2a481b9169adb34e8b61cbc152009d8435168ae7
Arshad <ahmed.a0611@gmail.com>
2023-08-22 23:21:15 +0530

    html changiine

--- demo/dom.js
+++ demo/dom.js
@@ -1,4 +1,11 @@
 // 1. Selection of an Element
+
+var a = document.querySelector("h1");
+console.log(a);
+
 // 2. Changing HTML
+
+var b = document.querySelector("h1");
+b.innerHTML = "DOM Started"
 // 3. Changing CSS
 // 4. Event Listener
